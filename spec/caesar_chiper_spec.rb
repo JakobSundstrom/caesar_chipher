dcd89348f638904523b0ad8551718b44b1b62147
JakobSundstrom <jakob.sundstrom1@gmail.com>
2023-08-30 14:04:02 +0200

    added and passed tests

--- spec/caesar_chiper_spec.rb
+++ spec/caesar_chiper_spec.rb
@@ -1,27 +1,27 @@
 require './lib/caesar_cipher'
 
 describe "#caesar_cipher" do
-    it "shifts letters in the string by the given factor" do
-      expect(caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
-    end
-  
-    it "wraps from z to a" do
-      expect(caesar_cipher("xyz", 3)).to eq("abc")
-    end
-  
-    it "preserves the case of letters" do
-      expect(caesar_cipher("AbCdEf", 1)).to eq("BcDeFg")
-    end
-  
-    it "leaves non-alphabetic characters unchanged" do
-      expect(caesar_cipher("Hello, World!", 7)).to eq("Olssv, Tlsva!")
-    end
-  
-    it "handles negative shifts" do
-      expect(caesar_cipher("Bmfy f xywnsl!", -5)).to eq("What a string!")
-    end
-  
-    it "handles shifts greater than 26" do
-      expect(caesar_cipher("What a string!", 30)).to eq("Bmfy f xywnsl!")
-    end
+  it "shifts letters in the string by the given factor" do
+    expect(caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
   end
+
+  it "wraps from z to a" do
+    expect(caesar_cipher("xyz", 3)).to eq("abc")
+  end
+
+  it "preserves the case of letters" do
+    expect(caesar_cipher("AbCdEf", 1)).to eq("BcDeFg")
+  end
+
+  it "leaves non-alphabetic characters unchanged" do
+    expect(caesar_cipher("Hello, World!", 7)).to eq("Olssv, Dvysk!")
+  end
+
+  it "handles negative shifts" do
+    expect(caesar_cipher("Bmfy f xywnsl!", -5)).to eq("What a string!")
+  end
+
+  it "handles shifts greater than 26" do
+    expect(caesar_cipher("What a string!", 31)).to eq("Bmfy f xywnsl!")
+  end
+end
